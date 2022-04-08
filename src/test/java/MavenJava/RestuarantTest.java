b361d9e8dd77133c911a2a396ce9c9d816c34fd6
Anupam <banerjee.anupamkol@gmail.com>
2022-04-08 09:19:36 +0530

    second round

---
+++ src/test/java/MavenJava/RestuarantTest.java
@@ -0,0 +1,19 @@
+package MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestuarantTest {
+	
+	@Test
+	public void RestuarantLogin() {
+
+		System.out.println("RestuarantLogin");
+	}
+
+	@Test
+	public void RestuarantLogout() {
+
+		System.out.println("RestuarantLogout");
+	}
+
+}
